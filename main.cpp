110afeaed274542924e8b419fb0f1706ee1f04b7
Luis Ernesto Torres <0906564@MY.SCCCD.EDU>
2026-04-19 10:52:48 -0700

    mainly just used a bunch of cout statements to display messages for user to login and different options to pick from

--- main.cpp
+++ main.cpp
@@ -2,12 +2,58 @@
 // Author: [Luis Torres]
 
 /* 
- * Description: this is a test to see if commit work
- *
+ * Description: this is an inventory management system for personal collection tracker (trading cards)
+ * users get to input name of cards they are looking for. it should output the price, condition, and availability
+ * it should also sort from highest to lowest / lowest to highest
 */
 
 #include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
 
 int main() {
-    return 0;
+
+	int userName;
+	int choice;
+   
+	//this is to ask if they are existing user or a new user
+	//ask if they want to login
+	cout << " --- CARD COLLECTION --- " << endl;
+	cout << endl << "ENTER USERNAME TO LOGIN" << endl;
+	cout << endl << "ENTER QUIT TO EXIT PROGRAM" << endl;
+	
+
+
+
+
+
+
+	//setup for the interface for the inventory management system
+
+	cout << "      " << "WELCOME BACK   " << "username" << "                " << endl;
+	cout << "*********************************************" << endl;
+
+	//ask if it wants to buy, sell, trade, view orders, update cards
+	//make this into a while loop to press 1 for buy, 2. sell, etc and press x to quit out of program
+
+	cout << "1. buy" << endl;
+	// if user wants to buy, ask if they want to buy products or single cards
+
+
+	cout << "2. sell" << endl;
+	cout << "3. trade" << endl;
+	cout << "4. view orders" << endl;
+	cout << "5. update profile" << endl;
+
+	cout << "*********************************************" << endl;
+
+	cout << "ENTER YOUR CHOICE" << endl;
+
+
+
+		return 0;
+
 }
